fc27c63eeb91aec9d608f3d81e0b08d428cd6e1f
Edaf S <edafsarwar@gmail.com>
2016-05-20 16:51:56 -0400

    next commit
    bot.js
    botResponse = cool() ...
    botResponse = "string";

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "from postMessage()";
 
   options = {
     hostname: 'api.groupme.com',
