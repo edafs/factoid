b34fa0dfe0a19999ec298067e825ea131203285d
Edaf S <edafsarwar@gmail.com>
2016-07-10 16:41:29 -0400

    changes to test
    blehh...

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ function respond() {
 	var request = JSON.parse(this.req.chunks[0]),
 		botRegex = /^\*factoid$/;
 
-	if(request.text && botRegex.test(request.text)) {
-	//if(request.text && botRegex.test(request.text.includes("*factoid") )) {
+	//if(request.text && botRegex.test(request.text)) {
+	if(request.text && botRegex.test( request.text.test("*factoid") )) {
 	this.res.writeHead(200);
 	postMessage();
 	this.res.end();
