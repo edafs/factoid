9636e698a9afbb2d96913ef36b90aea12ed3e0db
Edaf S <edafsarwar@gmail.com>
2016-07-10 16:35:28 -0400

    changes to regex
    bleh

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@ var botID = process.env.BOT_ID;
 
 function respond() {
 	var request = JSON.parse(this.req.chunks[0]),
-		botRegex = /^\*factoid$/;
+		botRegex = "*factoid" ;//botRegex = /^\*factoid$/;
 
-	//if(request.text && botRegex.test(request.text)) {
-	if(request.text && botRegex.test(request.text.includes("*factoid") )) {
+	if(request.text && botRegex.test(request.text)) {
+	//if(request.text && botRegex.test(request.text.includes("*factoid") )) {
 	this.res.writeHead(200);
 	postMessage();
 	this.res.end();
