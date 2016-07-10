edff27777657c35dcb82de13a82f15607f965398
Edaf S <edafsarwar@gmail.com>
2016-07-10 16:37:36 -0400

    ehh
    asdf

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
 	var request = JSON.parse(this.req.chunks[0]),
-		botRegex = "*factoid" ;//botRegex = /^\*factoid$/;
+		botRegex = /^\*factoid$/;
 
 	if(request.text && botRegex.test(request.text)) {
 	//if(request.text && botRegex.test(request.text.includes("*factoid") )) {
