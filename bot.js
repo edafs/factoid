9fcb81b9e085bb8a493841ca1751d42bd02d95d4
Edaf S <edafsarwar@gmail.com>
2016-07-10 16:48:56 -0400

    blehhh
    asdflkajs

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
 	var request = JSON.parse(this.req.chunks[0]),
 		botRegex = /^\*factoid$/;
 
-	if( request.text && botRegex.test(request.text) ) {
+	if( request.text && botRegex.includes(request.text) ) {
 		this.res.writeHead(200);
 		postMessage();
 		this.res.end();
